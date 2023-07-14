bb8f233fa525669483c87728cce316fc653091f4
Jesus Tadeu <tadeu_bressiano@hotmail.com>
2023-07-14 12:36:36 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //somente esse a cima faz a mesma coisa doque o codigo abaixo
+
+  //pegar a tag img
+  //const img = document.querySelector("#profile img")
+
+  //substituir a foto
+  //if (html.classList.contains("light")) {
+  // se tiver light mode, adiciona a foto light
+  //img.setAttribute("src", "./Assest/assets/avatarlight.png.png")
+  //} else {
+  // set tiver sem light mode, manter a foto normal
+  // img.setAttribute("src", "./Assets/assets/avatar.png.png")
+  // }
+}
